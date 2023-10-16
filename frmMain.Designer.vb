df4df27b3790ce5401c030ed22eaf84562a14caa
GOSLJ20 <GOSLJ20@ST5402-GOSLJ20>
2023-10-16 22:08:28 +1100

    Add project files.

---
+++ frmMain.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        tmrTimer = New Timer(components)
+        btnToggle = New Button()
+        SuspendLayout()
+        ' 
+        ' tmrTimer
+        ' 
+        tmrTimer.Interval = 50
+        ' 
+        ' btnToggle
+        ' 
+        btnToggle.Font = New Font("Museo 700", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point)
+        btnToggle.Location = New Point(12, 12)
+        btnToggle.Name = "btnToggle"
+        btnToggle.Size = New Size(776, 426)
+        btnToggle.TabIndex = 0
+        btnToggle.Text = "Start"
+        btnToggle.UseVisualStyleBackColor = True
+        ' 
+        ' frmMain
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnToggle)
+        Name = "frmMain"
+        Text = "frmMain"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents tmrTimer As Timer
+    Friend WithEvents btnToggle As Button
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Media
+Imports System.Xml
+
+Public Class frmMain
+    Dim strCountdown As String = 10
+    Dim strTurn As String = "White"
+    Dim bStart As Boolean = False
+    Dim decBeginTime As Decimal = 10
+    Private Sub btnToggle_Click(sender As Object, e As EventArgs) Handles btnToggle.Click
+        If bStart = False Then
+            tmrTimer.Enabled = True
+            bStart = True
+        ElseIf strTurn = "White" Then
+            strTurn = "Black"
+            strCountdown = (decBeginTime - strCountdown) + 2
+            decBeginTime = strCountdown
+        ElseIf strTurn = "Black" Then
+            strTurn = "White"
+            strCountdown = (decBeginTime - strCountdown) + 2
+            decBeginTime = strCountdown
+        End If
+
+    End Sub
+
+
+    Private Sub tmrTimer_Tick(sender As Object, e As EventArgs) Handles tmrTimer.Tick
+        strCountdown = FormatCurrency(strCountdown - 0.05)
+        strCountdown = strCountdown.Replace("$", "")
+        If strCountdown > 0 Then
+            If strTurn = "White" Then
+            btnToggle.BackColor = Color.White
+            btnToggle.ForeColor = Color.Black
+            btnToggle.Text = "White's Turn" & vbCrLf & strCountdown
+        End If
+        If strTurn = "Black" Then
+            btnToggle.BackColor = Color.Black
+            btnToggle.ForeColor = Color.White
+            btnToggle.Text = "Black's Turn" & vbCrLf & strCountdown
+        End If
+        ElseIf strCountdown <= 0 Then
+        If strTurn = "Black" Then
+                btnToggle.BackColor = Color.White
+                btnToggle.ForeColor = Color.Black
+                btnToggle.Text = "White Wins"
+                strTurn = "End"
+                tmrTimer.Enabled = False
+            ElseIf strTurn = "White" Then
+                btnToggle.BackColor = Color.Black
+                btnToggle.ForeColor = Color.White
+                btnToggle.Text = "Black Wins"
+                strTurn = "End"
+                tmrTimer.Enabled = False
+            Else
+                btnToggle.BackColor = Color.LightGray
+                btnToggle.ForeColor = Color.Black
+                btnToggle.Text = "Error"
+            End If
+        End If
+    End Sub
+End Class
